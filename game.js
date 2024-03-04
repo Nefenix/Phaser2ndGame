4cc7ff1b973c667ecb8c7a550dc294ae6bd0106b
Nefenix <23a_kiv@liceum.ztu.edu.ua>
2024-03-04 10:19:15 +0200

    Обнова
    ОБновааааУУУААА

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var config = { //Налаштовуємо сцену
     type: Phaser.AUTO,
-    width: 10000,
+    width: 5000,
     height: 1080,
     pixelArt: true,
     debug: true,
@@ -26,7 +26,8 @@ var scoreText;
 var record = 0
 
 //Функція підбору зірок
-function collectKiwi(player, kiwi) {
+function collectKiwi (player, kiwi)
+{
     kiwi.disableBody(true, true);
 
     //Нараховуємо бали
@@ -41,7 +42,8 @@ function collectKiwi(player, kiwi) {
 
 
     //Перестворення зірочок
-    if (kiwis.countActive(true) === 0) {
+    if (kiwis.countActive(true) === 0)
+    {
         kiwis.children.iterate(function (child) {
 
             child.enableBody(true, child.x, 0, true, true);
@@ -54,7 +56,8 @@ function collectKiwi(player, kiwi) {
 }
 
 //Після зіткнення 3
-function hitBomb(player, bomb) {
+function hitBomb (player, bomb)
+{
     this.physics.pause();
     player.setTint(0xff0000);
     player.anims.play('turn');
@@ -62,34 +65,35 @@ function hitBomb(player, bomb) {
 
     this.scene.restart();
 
-    if (score > record) {
+    if(score > record){
         record = score;
         document.getElementById("record_text").innerHTML = "Рекорд :<br/>" + record;
     }
     score = 0
 }
 
-function preload() //Завантажуємо графіку для гри
+function preload () //Завантажуємо графіку для гри
 {
-    this.load.image('ground', 'assets/platform.png');
+    this.load.image('cheese', 'assets/cheese.png');
+    this.load.image('ground', 'assets/ground.png');
     this.load.image('sky', 'assets/sky.png');
     this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude',
+    this.load.spritesheet('dude', 
         'assets/dude.png',
         { frameWidth: 32, frameHeight: 32 }
     );
-    this.load.spritesheet('dudeleft',
+    this.load.spritesheet('dudeleft', 
         'assets/dudeleft.png',
         { frameWidth: 32, frameHeight: 32 }
     );
 }
 
-function create() {
-    //Додаемо небо
-   // for (var n = 0; n < 10; n++) {
-this.add.image(960, 540, 'sky').setScale(1);
-    
 
+function create ()
+{
+    
+    //Додаемо небо
+    this.add.image(960, 540, 'sky').setScale(1);
 
     //Створюемо текст з рахунком
     scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
@@ -102,21 +106,13 @@ this.add.image(960, 540, 'sky').setScale(1);
 
     //Створюемо платформи
     platforms.create(955, 880, 'ground').refreshBody().setScale(1);
-
-
+    
+    
 
     //Створюємо та налаштовуємо спрайт гравця
     player = this.physics.add.sprite(100, 0, 'dude').setScale(2);
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
-
-    //Camera
-
-
-    this.cameras.main.setBounds(0, 0, worldWidth 10000, window.innerHeight);
-    this.physics.world.setBounds(0, 0, worldWidth, window.innerHeight);
-
-    this.cameras.main.startFollow(player);
 
     //Створюемо та налаштовуємо фізичний об'єкт бомби
     bombs = this.physics.add.group();
@@ -137,7 +133,7 @@ this.add.image(960, 540, 'sky').setScale(1);
 
     this.anims.create({
         key: 'turn',
-        frames: [{ key: 'dude', frame: 4 }],
+        frames: [ { key: 'dude', frame: 4 } ],
         frameRate: 20
     });
 
@@ -151,24 +147,31 @@ this.add.image(960, 540, 'sky').setScale(1);
     //Додано колізії між гравцем та платформами
     this.physics.add.collider(player, platforms);
 
+    this.cameras.main.setBounds(0, 0, 5000, 1080); // Встановлюємо межі камери на всю ширину та висоту сцени
+this.cameras.main.startFollow(player);
 }
 
-function update() {
+function update ()
+{
     //Керування персонажем
-    if (cursors.left.isDown) {
+    if (cursors.left.isDown)
+    {
         player.setVelocityX(-160);
         player.anims.play('left', true);
     }
-    else if (cursors.right.isDown) {
+    else if (cursors.right.isDown)
+    {
         player.setVelocityX(160);
         player.anims.play('right', true);
     }
-    else {
+    else
+    {
         player.setVelocityX(0);
         player.anims.play('turn');
     }
 
-    if (cursors.up.isDown && player.body.touching.down) {
+    if (cursors.up.isDown && player.body.touching.down)
+    {
         player.setVelocityY(-330);
     }
 }
